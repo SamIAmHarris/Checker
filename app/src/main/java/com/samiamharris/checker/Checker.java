7792fa4f8cc78026615c5a982313822614d07979
Sam Harris <SamIAm@Sams-MacBook-Pro-2.local>
2018-02-11 10:14:18 -0500

    Setup data models for checker and streak items.

---
+++ app/src/main/java/com/samiamharris/checker/Checker.java
@@ -0,0 +1,11 @@
+package com.samiamharris.checker;
+
+import java.util.Date;
+
+/**
+ * Created by SamIAm on 2/11/18.
+ */
+
+public class Checker {
+    Date date;
+}
---
+++ app/src/main/java/com/samiamharris/checker/Streak.java
@@ -0,0 +1,14 @@
+package com.samiamharris.checker;
+
+import java.util.Date;
+
+/**
+ * Created by SamIAm on 2/11/18.
+ */
+
+public class Streak {
+
+    Date startTime;
+    Date endTime;
+    
+}
